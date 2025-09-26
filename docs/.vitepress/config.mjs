2e138316a86776ee2f59258caab886281096192f
laibinglin <1154246882@qq.com>
2025-09-26 08:46:54 +0800

    update

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -19,7 +19,20 @@ export default defineConfig({
         items: [{ text: "上传与下载", link: "/JavaScript/上传与下载/上传" }],
       },
       { text: "package", link: "/package/unplugin-auto-import" },
-      { text: "Vue", items: [{ text: "pinia", link: "/vue/pinia/action" }] },
+      {
+        text: "Vue",
+        items: [
+          { text: "pinia", link: "/vue/pinia/action" },
+          {
+            text: "Vue2",
+            items: [{ text: "全局属性", link: "/vue/vue2/全局属性" }],
+          },
+          {
+            text: "Vue3",
+            items: [{ text: "全局属性", link: "/vue/vue3/全局属性" }],
+          },
+        ],
+      },
     ],
     outline: "deep",
     outlineTitle: "目录",
