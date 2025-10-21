b3974c5cdea6f34f24f08aea9f18c7ca161f0a1a
laibinglin <1154246882@qq.com>
2025-10-21 14:56:46 +0800

    update flex

---
+++ docs/snippets/flex-order.vue
@@ -0,0 +1,18 @@
+<script setup></script>
+
+<template>
+  <div class="button-group">
+    <button class="btn">取消</button>
+    <button class="btn">重置</button>
+    <button class="btn primary">确定</button>
+  </div>
+</template>
+
+<style scoped>
+.button-group {
+  display: flex;
+}
+.primary {
+  order: -1;
+}
+</style>
